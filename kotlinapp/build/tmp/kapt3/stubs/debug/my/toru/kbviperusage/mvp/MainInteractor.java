2b02add919b8b41a24b09044957b68e2b7f2aa18
Toru Choi <toru.choi@11street.my>
2017-10-25 11:40:26 +0800

    deleted tmp files

--- kotlinapp/build/tmp/kapt3/stubs/debug/my/toru/kbviperusage/mvp/MainInteractor.java
+++
@@ -1,5 +0,0 @@
-package my.toru.kbviperusage.mvp;
-
-@kotlin.Metadata(mv = {1, 1, 7}, bv = {1, 0, 2}, k = 1, d1 = {"\u0000\u000e\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\bf\u0018\u00002\b\u0012\u0004\u0012\u00020\u00020\u0001\u00a8\u0006\u0003"}, d2 = {"Lmy/toru/kbviperusage/mvp/MainInteractor;", "Ltoru/io/my/bviper/interactor/BViperInteractor;", "Lmy/toru/kbviperusage/model/requestmodel/RequestModel;", "kotlinapp_debug"})
-public abstract interface MainInteractor extends toru.io.my.bviper.interactor.BViperInteractor<my.toru.kbviperusage.model.requestmodel.RequestModel> {
-}
--- kotlinapp/build/tmp/kapt3/stubs/debug/my/toru/kbviperusage/mvp/MainInteractorOut.java
+++
@@ -1,5 +0,0 @@
-package my.toru.kbviperusage.mvp;
-
-@kotlin.Metadata(mv = {1, 1, 7}, bv = {1, 0, 2}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\bf\u0018\u00002\u000e\u0012\n\u0012\b\u0012\u0004\u0012\u00020\u00030\u00020\u0001\u00a8\u0006\u0004"}, d2 = {"Lmy/toru/kbviperusage/mvp/MainInteractorOut;", "Ltoru/io/my/bviper/interactor/BViperInteractorOut;", "", "Lmy/toru/kbviperusage/model/response/InstagramItemModel;", "kotlinapp_debug"})
-public abstract interface MainInteractorOut extends toru.io.my.bviper.interactor.BViperInteractorOut<java.util.List<? extends my.toru.kbviperusage.model.response.InstagramItemModel>> {
-}
